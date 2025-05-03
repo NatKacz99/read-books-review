ec07f8506d31035d0caa333b7a97012a94667a22
Natalia Kaczyńska <natalia.kaczynska.programista@gmail.com>
2025-05-03 13:00:29 +0200

    add initial view

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "books_review",
+  password: "Josr84#",
+  port: 5432
+})
+
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const URL_API = "https://covers.openlibrary.org/b/";
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
