117d302b2f0f94d73bda71c0a8e6782ad5d6700a
Natalia Kaczyńska <natalia.kaczynska.programista@gmail.com>
2025-05-04 13:37:59 +0200

    add search by a genre (correct)

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-async function selectAllBooks(){
+async function selectAllBooks() {
   const result = await db.query("SELECT * FROM books");
   const books = result.rows;
   return books;
@@ -26,30 +26,41 @@ async function selectAllBooks(){
 
 app.get("/", async (req, res) => {
   const books = await selectAllBooks();
-  res.render("index.ejs", {books});
+  res.render("index.ejs", 
+    { books,
+      kindOfSort: null,
+      genre: null
+     });
 });
 
-app.get("/sort/:options", async (req, res) => {
-  const selectedOption = req.params.options;
-  const result = await db.query(`SELECT * FROM books WHERE
-    genre LIKE $1`, [`%${selectedOption}`]);
-  const books = result.rows
-  res.render("index.ejs", {books})
+app.get("/sort/:kindOfSort/:genre", async (req, res) => {
+  const selectedKindOfSort = req.params.kindOfSort;
+  const selectedOptionGenre = req.params.genre;
+  if(selectedKindOfSort === "gatunkiem"){
+    const result = await db.query(`SELECT * FROM books WHERE
+        genre LIKE $1`, [`%${selectedOptionGenre}`]);
+    const books = result.rows;
+    res.render("index.ejs", { 
+      books,
+      kindOfSort: selectedKindOfSort,
+      genre: selectedOptionGenre
+    })
+  }
 })
 
 app.post("/search", async (req, res) => {
-  try{
-  const searchedPhrase = req.body.search;
-  const searchTerm = `%${searchedPhrase.toLowerCase()}%`;
-  const result = await db.query(`SELECT * FROM books WHERE
+  try {
+    const searchedPhrase = req.body.search;
+    const searchTerm = `%${searchedPhrase.toLowerCase()}%`;
+    const result = await db.query(`SELECT * FROM books WHERE
     LOWER(title) LIKE $1 OR LOWER(author) LIKE $1`, [searchTerm]);
-  const books = result.rows;
-  if(books.length === 0){
-    const error = "Brak książek odpowiadających szukanej frazie.";
-    res.render("index.ejs", {books, error: error});
-    return;
-  }
-  res.render("index.ejs", {books, error: ""})
+    const books = result.rows;
+    if (books.length === 0) {
+      const error = "Brak książek odpowiadających szukanej frazie.";
+      res.render("index.ejs", { books, error: error });
+      return;
+    }
+    res.render("index.ejs", { books, error: "" })
   } catch (err) {
     console.log(err);
     res.status(500).send("Error retrieving data.");
