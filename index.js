52d4e9103ef75d7e1a1613a5c9a7c33716a0ae61
Natalia Kaczyńska <natalia.kaczynska.programista@gmail.com>
2025-05-04 14:52:54 +0200

    add sort by a rate and correts other parts of sort

--- index.js
+++ index.js
@@ -26,33 +26,73 @@ async function selectAllBooks() {
 
 app.get("/", async (req, res) => {
   const books = await selectAllBooks();
-  res.render("index.ejs", 
-    { books,
+  res.render("index.ejs",
+    {
+      books,
       kindOfSort: null,
       genre: null
-     });
+    });
 });
+
+app.get("/sort/:kindOfSort", async (req, res, next) => {
+  const kindOfSort = req.params.kindOfSort;
+  if (kindOfSort === 'oceną') {
+    try {
+      const result = await db.query(`
+        SELECT b.*, r.rate_digit
+          FROM books AS b
+          JOIN rates AS r
+            ON b.book_id = r.book_id
+        ORDER BY r.rate_digit DESC
+      `);
+      const books = result.rows;
+      return res.render("index.ejs", {
+        books,
+        kindOfSort
+      });
+    } catch (err) {
+      return next(err);
+    }
+  }
+  if (kindOfSort === 'tytułem') {
+    try {
+      const result = await db.query(`SELECT * FROM books ORDER BY title ASC`)
+      const books = result.rows;
+      res.render("index.ejs", {
+        books,
+        kindOfSort
+      })
+    } catch (err) {
+      return next(err);
+    }
+  }
+});
+
 
 app.get("/sort/:kindOfSort", async (req, res) => {
   const selectedKindOfSort = req.params.kindOfSort;
-  if(selectedKindOfSort === "tytułem"){
-    const result = await db.query(`SELECT * FROM books ORDER BY title ASC`)
-    const books = result.rows;
-    res.render("index.ejs", { 
-      books,
-      kindOfSort: selectedKindOfSort
-    })
+  try {
+    if (selectedKindOfSort === "tytułem") {
+      const result = await db.query(`SELECT * FROM books ORDER BY title ASC`)
+      const books = result.rows;
+      res.render("index.ejs", {
+        books,
+        kindOfSort: selectedKindOfSort
+      })
+    }
+  } catch (err) {
+    console.error("Error during insertion:", err);
   }
 })
 
 app.get("/sort/:kindOfSort/:genre", async (req, res) => {
   const selectedKindOfSort = req.params.kindOfSort;
   const selectedOptionGenre = req.params.genre;
-  if(selectedKindOfSort === "gatunkiem"){
+  if (selectedKindOfSort === "gatunkiem") {
     const result = await db.query(`SELECT * FROM books WHERE
         genre LIKE $1`, [`%${selectedOptionGenre}`]);
     const books = result.rows;
-    res.render("index.ejs", { 
+    res.render("index.ejs", {
       books,
       kindOfSort: selectedKindOfSort,
       genre: selectedOptionGenre
